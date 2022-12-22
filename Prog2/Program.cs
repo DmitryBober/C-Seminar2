f1b0964d2d642bc33e8f78bb253e9df6919022de
Dmitry Bober <bober113d@gmail.com>
2022-12-22 19:02:07 +0500

    prog2

---
+++ Prog2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int num = 38900;
+Console.WriteLine(num);
+string str = num.ToString();
+
+if (num == 38900)
+{
+    Console.WriteLine(str[2]);
+}
+ else 
+ {
+    Console.Write("Третьей цифры нет:(");
+ }
