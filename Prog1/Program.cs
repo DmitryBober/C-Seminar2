12ed08a1da583bd92c88e3d93a3fb046a9155163
Dmitry Bober <bober113d@gmail.com>
2022-12-22 18:42:25 +0500

    prog1.1

--- Prog1/Program.cs
+++ Prog1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-int num = 279;
+int num = 589;
 Console.WriteLine(num);
-string str = 279.ToString();
+string str = num.ToString();
 Console.WriteLine(str[1]);
